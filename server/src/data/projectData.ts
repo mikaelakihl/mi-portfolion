6d9ec81b39c19a594ee03e1b8467bec14a1a56a3
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-11-16 15:01:43 +0100

    feat: add Projects in server and add img

---
+++ server/src/data/projectData.ts
@@ -0,0 +1,106 @@
+import { IProject } from '../types/project.js';
+
+export const projectData: IProject[] = [
+  {
+    id: 0,
+    title: 'Gottfrids canvas shop',
+    img: {
+      src: '/assets/projects/gottfrids-canvas-shop-portfolio.png',
+      alt: 'quiz',
+      width: 1466,
+      height: 817
+    },
+    description: `This project was developed as a school project at Medieinstitutet.
+        The aim was to create a webshop for donuts or another product of choice. I chose to create a webshop for Canvases I've created in photoshop a few years ago to make it a little bit more personal.
+        Troughtout this project I focused on understanding the basics of Java Script and I'm excited to redo this projects in a few years. This is my first project so I consider it to be the most important one. I hope you enjoy.`,
+    tech: ['HTML', 'CSS', 'JavaScript', 'Sass', 'Prettier'],
+    demo: 'https://mikaelakihl.github.io/mi-js-intro-assignment1-webshop/',
+    github: 'https://github.com/mikaelakihl/mi-js-intro-assignment1-webshop',
+    created_at: ''
+  },
+  {
+    id: 1,
+    title: 'Cartoon quiz',
+    img: {
+      src: '/assets/projects/cartoon-quiz-portfolio.png',
+      alt: 'quiz',
+      width: 1718,
+      height: 1000
+    },
+    description: `This project was developed as a school group-project at Medieinstitutet.
+        The aim was to create a quiz in theme of choice. We chose to create a quiz about cartoons because it felt fun to play with happy colors.
+        Troughout this project we focused on learning how to work as a group, both individually and pair coding. I hope you enjoy.`,
+    tech: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Sass', 'Prettier', 'EsLint', 'Vite'],
+    demo: 'https://mikaelakihl.github.io/mi-js-intro-assignment2-cartoonquiz/',
+    github: 'https://github.com/mikaelakihl/mi-js-intro-assignment2-cartoonquiz',
+    created_at: ''
+  },
+  {
+    id: 3,
+    title: 'Tough Cookies',
+    img: {
+      src: '/assets/projects/tough-cookies-portfolio.png',
+      alt: 'tough cookies',
+      width: 1991,
+      height: 1113
+    },
+    description: `This project was developed as a school project at Medieinstitutet.
+        We were given a pre-made design to replicate using HTML and CSS for the first time.`,
+    tech: ['HTML', 'CSS', 'Sass'],
+    demo: 'https://mikaelakihl.github.io/mi-html-css-assignment2-toughcookie/',
+    github: 'https://github.com/mikaelakihl/mi-html-css-assignment2-toughcookie',
+    created_at: ''
+  },
+  {
+    id: 4,
+    title: 'Jurassic World - Rebirth',
+    img: {
+      src: '/assets/projects/jurassic-world-portfolio.png',
+      alt: 'jurassic world',
+      width: 1848,
+      height: 1000
+    },
+    description: `Welcome to a "Design-Swap", an assignment we did while studying the Front End Developer Program at Mediainstitutet. 
+        In this project we got handed a desing from another group and coded the website acording to their vision. `,
+    tech: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Sass', 'Prettier', 'EsLint', 'Vite', 'Vue', 'Node.js', 'Pnpm'],
+    demo: 'https://mikaelakihl.github.io/mi-graphic-tools-assignment2-jurassic-world/',
+    github: 'https://github.com/mikaelakihl/mi-graphic-tools-assignment2-jurassic-world',
+    created_at: ''
+  },
+  {
+    id: 5,
+    title: 'Reptilicious',
+    img: {
+      src: '/assets/projects/reptilicous-portfolio.png',
+      alt: 'reptilicious',
+      width: 1833,
+      height: 1003
+    },
+    description: `Welcome to the other project of the "Design-Swap", an assignment we did while studying the Front End Developer Program at Mediainstitutet. 
+        In this project we handed over this design and another group code the website acording to our vision. `,
+    tech: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Sass', 'Prettier', 'EsLint', 'Vite', 'Vue', 'Node.js', 'Npm'],
+    demo: 'https://mikaelakihl.github.io/mi-graphic-tools-assignment1-reptilicious/',
+    github: 'https://github.com/mikaelakihl/mi-graphic-tools-assignment1-reptilicious',
+    created_at: ''
+  },
+  {
+    id: 6,
+    title: 'The Lumos Library',
+    img: {
+      src: '/assets/projects/the-lumos-library-portfolio.png',
+      alt: 'The lumos library',
+      width: 1670,
+      height: 1053
+    },
+    description: `In this project we built a Book-api with MongoDB as backend and a Frontend interface with Vue. It allows us to browse, review and manage a collection of books. 
+        The application also includes user authentication, admin-only access for managing content, and public access for viewing books submitting reviews.<br></br>
+        Log in as admin, use<br>
+        <b>username:</b> admin<br>
+        <b>password:</b> admin123`,
+    tech: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Sass', 'Vite', 'Vue', 'Node.js', 'Npm', 'Express', 'MongoDB'],
+    demo: 'https://mi-api-development-assignment2-the-lumos-library-client.vercel.app/',
+    github: 'https://github.com/mikaelakihl/mi-api-development-assignment2-the-lumos-library',
+    created_at: ''
+  }
+];
+
---
+++ server/src/types/project.ts
@@ -0,0 +1,16 @@
+export interface IProject {
+  id: number;
+  title: string;
+  img?: {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+  };
+  description: string;
+  tech: string[];
+  demo: string;
+  github: string;
+  created_at: string;
+}
+
