36829abfd37c404d42872a0dae6f9dcd3f0ce7ca
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-11-22 14:38:43 +0100

    fix: refine layout and add drop shadows to images in App.tsx for enhanced visual appeal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -45,7 +45,7 @@ const CV = () => {
     <div className='flex md:flex-row flex-col'>
       <SideBarCV/>
       <div className='flex flex-col w-full' >
-        <div className='flex-1 md:mt-20'>
+        <div className='flex-1 md:mt-20 mt-20'>
           <Cloud />
         </div>
         <div className='flex-1'>
@@ -63,10 +63,10 @@ const Cloud = () => {
     
     <div className='bg-brand-100 relative h-full'>
       <div className='absolute inset-3 flex items-center gap-2 -translate-y-[10%]'>
-        <img src="/assets/art/flysland.png" alt="floating island" className="w-full h-full object-contain -rotate-20 mt-[5%] mx-[2%] translate-x-[20%] md:translate-x-[9%]" />
-        <img src="/assets/art/no_one_is_captain_but_you.png" alt="floating island" className="w-full h-full mb-10 object-contain -rotate-6 -translate-x-[8%]" />
-        <img src="/assets/art/djungle_2.png" alt="floating island" className="w-full h-full object-contain rotate-6 mb-[12%] -translate-x-[20%]" />
-        <img src="/assets/art/caravan_dolphin.png" alt="floating island" className="w-full h-full object-contain rotate-15 mt-[5%] mx-[-4%] -translate-x-[25%] md:-translate-x-[20%]" />
+        <img src="/assets/art/flysland.png" alt="floating island" className="w-full h-full object-contain -rotate-20 mt-[5%] mx-[2%] translate-x-[20%] md:translate-x-[9%] filter-[drop-shadow(0_0_1px_rgba(0,0,0,0.3))_drop-shadow(0_4px_6px_rgba(0,0,0,0.3))]" />
+        <img src="/assets/art/no_one_is_captain_but_you.png" alt="floating island" className="w-full h-full mb-10 object-contain -rotate-6 -translate-x-[8%] filter-[drop-shadow(0_0_1px_rgba(0,0,0,0.3))_drop-shadow(0_4px_6px_rgba(0,0,0,0.3))]" />
+        <img src="/assets/art/djungle_2.png" alt="floating island" className="w-full h-full object-contain rotate-6 mb-[12%] -translate-x-[20%] filter-[drop-shadow(0_0_1px_rgba(0,0,0,0.3))_drop-shadow(0_4px_6px_rgba(0,0,0,0.3))]" />
+        <img src="/assets/art/caravan_dolphin.png" alt="floating island" className="w-full h-full object-contain rotate-15 mt-[5%] mx-[-4%] -translate-x-[25%] md:-translate-x-[20%] filter-[drop-shadow(0_0_1px_rgba(0,0,0,0.3))_drop-shadow(0_4px_6px_rgba(0,0,0,0.3))]" />
       </div>
       <img src="/assets/cloud.png" alt="cloud" className="w-full h-full relative z-20" /> 
     </div>
@@ -157,7 +157,7 @@ const MainCV = () => {
   if (!cvData) return <div>No data available</div>;
 
   return (
-    <section className='flex flex-col bg-white gap-4 w-full px-4 text-left md:p-20 relative z-10'>
+    <section className='flex flex-col bg-white gap-4 w-full px-4 pb-10 pt-10 text-left md:p-20 relative z-10'>
       <div className='flex flex-col gap-4 w-full mt-4' >
         <p>My career started when I was 16 and during these years I've gained a lot of skills, socially and practically. Five words to describe myself, <strong>open, loyal, responsible, inventive & adaptable.</strong></p>
         <p>I enjoy working in a team but I'm also very independent. After five years in customer support I'm ready for new challenges in Front End Development.</p>
