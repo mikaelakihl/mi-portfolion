4739a22b905fda13c3fb05d0ae66725b85392eca
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-11-25 14:15:25 +0100

    feat: add navigation link to art section in Cloud component of App.tsx, enhancing user access to art content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -123,10 +123,12 @@ const CV = () => {
 
 
 const Cloud = () => {
+  
   return (
     <>
-    
+    <NavLink to="/art">
     <div className=' relative h-full'>
+      
       <div className='absolute inset-3 flex items-center gap-2 -translate-y-[10%]'>
         <img src="/assets/art/flysland.png" alt="floating island" className="w-full h-full object-contain -rotate-20 mt-[5%] mx-[2%] translate-x-[20%] md:translate-x-[9%] filter-[drop-shadow(0_0_1px_rgba(0,0,0,0.3))_drop-shadow(0_4px_6px_rgba(0,0,0,0.3))]" />
         <img src="/assets/art/no_one_is_captain_but_you.png" alt="floating island" className="w-full h-full mb-10 object-contain -rotate-6 -translate-x-[8%] filter-[drop-shadow(0_0_1px_rgba(0,0,0,0.3))_drop-shadow(0_4px_6px_rgba(0,0,0,0.3))]" />
@@ -135,6 +137,7 @@ const Cloud = () => {
       </div>
       <img src="/assets/cloud.png" alt="cloud" className="w-full h-full relative z-20" /> 
     </div>
+    </NavLink>
     </>
   )
   
