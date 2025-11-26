128a05c139f04d386be4838a506222df743286f3
Rtskihl <mikaela.kihl@medieinstitutet.se>
2025-11-26 15:29:54 +0100

    style: enhance link hover effects in App.tsx and centralize transition styles in index.css for improved visual consistency and user experience

--- src/App.tsx
+++ src/App.tsx
@@ -69,21 +69,21 @@ const Header = () => {
         <div className="hidden md:flex justify-center gap-8 bg-white/10 backdrop-blur-md border border-white/20 p-6 rounded-xl shadow-lg text-white ">
           <Link
             to="/"
-            className="text-white hover:text-brand-400 transition-colors"
+            className="text-white transition-colors duration-300 hover:text-orange-400"
           >
             CV
           </Link>
           <span className="text-black">|</span>
           <Link
             to="/projects"
-            className="text-white hover:text-brand-400 transition-colors"
+            className="text-white transition-colors duration-300 hover:text-orange-400"
           >
             Projects
           </Link>
           <span className="text-black">|</span>
           <Link
             to="/art"
-            className="text-white hover:text-brand-400 transition-colors"
+            className="text-white transition-colors duration-300 hover:text-orange-400"
           >
             Art
           </Link>
