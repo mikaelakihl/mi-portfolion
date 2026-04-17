7cedca46d8e6dc3d2f49faa56d7cc74fdc32b7c3
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-04-17 23:12:36 +0200

    style: enhance layout and spacing in AboutMe component for improved readability and visual appeal

--- src/routes/AboutMe.tsx
+++ src/routes/AboutMe.tsx
@@ -10,34 +10,39 @@ const AboutMe = () => {
       <div className="bg-gradient-to-r from-transparent via-white to-transparent h-[1px] my-6"></div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-white bg-white/10 backdrop-blur-md border border-white/20 p-6 md:px-10 rounded-xl shadow-lg text-white text-left">
         <div className="flex flex-col gap-4 w-full mt-4 text-white   ">
-         
-           
-            <h3 className="uppercase tracking-wider text-brand-600 font-bold text-base text-center">{t("Cv.about_me_title.2")}
+          <div className="space-y-4">
+             <h3 className="uppercase tracking-wider text-brand-600 font-bold text-base text-center">{t("Cv.about_me_title.2")}
                 </h3>
-              <p>{t("Cv.text_about_me.1")}</p>
-              <p>{t("Cv.text_about_me.1.1")}</p>
-          <details>
+                <div className="flex flex-col gap-2 mt-4">
+                 <p>{t("Cv.text_about_me.1")}</p>
+              <p>{t("Cv.text_about_me.1.1")}</p> 
+                </div>
+              
+          </div>
+              
+          <details className="group">
           <summary className="uppercase tracking-wider text-brand-600 font-bold text-base text-center">{t("Cv.about_me_title.3")}</summary>
-          <div className="flex flex-col gap-2"> 
+          <div className="flex flex-col gap-2 group-open:mt-4"> 
           <p>{t("Cv.text_about_me.2")}</p>
           <p>{t("Cv.text_about_me.2.1")}</p>
           </div>
           </details>
-          <details>
+          <details className="group">
           <summary className="uppercase tracking-wider text-brand-600 font-bold text-base text-center ">{t("Cv.about_me_title.4")}</summary>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 group-open:mt-4">
           <p>{t("Cv.text_about_me.3")}</p>
           <p>{t("Cv.text_about_me.3.1")}</p>
           </div>
           </details>
-         
+         <div>
           <h3 className="uppercase tracking-wider text-brand-600 font-bold text-base text-center">{t("Cv.about_me_title.5")}</h3>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 mt-4">
             <p>{t("Cv.text_about_me.4")}</p>
           <p>{t("Cv.text_about_me.4.1")}</p>
-          <p>{t("Cv.text_about_me.6")}</p>
+          <p>{t("Cv.text_about_me.6")}</p> 
           </div>
-         
+          
+         </div>
     
 
           
